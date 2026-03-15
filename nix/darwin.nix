793592b817b458963c87c43c322f3d6c489e3669
Naoki Takahashi <43571743+nemuki@users.noreply.github.com>
2026-03-15 20:23:54 +0900

    macOSのシステム設定を更新し、Finderの表示設定やDockのカスタマイズを追加。Touch IDによるsudo認証を有効化し、zshをデフォルトシェルとして設定。

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -18,6 +18,80 @@
     ];
   };
 
+  # Shell / Users
+  programs.zsh.enable = true;
+
+  users.users.${username} = {
+    home = homedir;
+    shell = pkgs.zsh;
+  };
+
+  # Enable Touch ID for sudo (including tmux support via pam-reattach)
+  security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.reattach = true;
+
+  # macOS system defaults
+  system = {
+    stateVersion = 5;
+    primaryUser = username;
+
+    defaults = {
+      dock = {
+        autohide = false;
+        tilesize = 40; # アイコンサイズ
+        show-recents = false; # 最近使ったアプリを表示しない
+        mru-spaces = false; # 最近使ったスペースを表示しない
+        mineffect = "scale"; # ウィンドウを開くときのアニメーション
+        minimize-to-application = true; # ウィンドウをアプリケーションアイコンに格納
+        persistent-others = [
+          # 最近使ったアプリを表示するスペース
+          {
+            folder = {
+              path = "/Users/${username}/Desktop";
+              showas = "fan";
+            };
+          }
+          {
+            folder = {
+              path = "/Users/${username}/Downloads";
+              showas = "fan";
+            };
+          }
+        ];
+        wvous-bl-corner = 11; # 左下のホットコーナーを「Launchpad」に設定
+        wvous-tr-corner = 12; # 右上のホットコーナーを「通知センター」に設定
+      };
+
+      # Finder 設定
+      finder = {
+        AppleShowAllExtensions = true; # 拡張子を常に表示
+        ShowPathbar = true;
+        ShowStatusBar = false;
+        FXPreferredViewStyle = "clmv"; # リスト表示をデフォルトにする
+        NewWindowTarget = "Home"; # 新しい Finder ウィンドウをホームディレクトリにする
+      };
+
+      NSGlobalDomain = {
+        AppleInterfaceStyleSwitchesAutomatically = true; # ダークモードを自動で切り替える
+        InitialKeyRepeat = 15; # キーリピートの初期遅延を短くする
+        KeyRepeat = 2; # キーリピートの速度を速くする
+        _HIHideMenuBar = false; # メニューバーを常に表示
+      };
+
+      trackpad = {
+        Clicking = true; # タップでクリック
+        TrackpadRightClick = true; # 2本指で右クリック
+      };
+
+      screencapture = {
+        disable-shadow = true; # スクリーンショットの影を消す
+        type = "png"; # スクリーンショットのファイル形式
+      };
+
+      menuExtraClock.ShowSeconds = true; # メニューバーの時計に秒を表示
+    };
+  };
+
   # System packages
   environment.systemPackages = with pkgs; [
     git
@@ -57,72 +131,4 @@
       "The Unarchiver" = 425424353;
     };
   };
-
-  # macOS system defaults
-  system.defaults = {
-    dock = {
-      autohide = false;
-      tilesize = 40; # アイコンサイズ
-      show-recents = false; # 最近使ったアプリを表示しない
-      mru-spaces = false; # 最近使ったスペースを表示しない
-      mineffect = "scale"; # ウィンドウを開くときのアニメーション
-      minimize-to-application = true; # ウィンドウをアプリケーションアイコンに格納
-      persistent-others = [
-        # 最近使ったアプリを表示するスペース
-        {
-          folder = {
-            path = "/Users/${username}/Desktop";
-            showas = "fan";
-          };
-        }
-        {
-          folder = {
-            path = "/Users/${username}/Downloads";
-            showas = "fan";
-          };
-        }
-      ];
-      wvous-bl-corner = 11; # 左下のホットコーナーを「Launchpad」に設定
-      wvous-tr-corner = 12; # 右上のホットコーナーを「通知センター」に設定
-    };
-
-    # Finder 設定
-    finder = {
-      AppleShowAllExtensions = true; # 拡張子を常に表示
-      ShowPathbar = true;
-      ShowStatusBar = false;
-      FXPreferredViewStyle = "clmv"; # リスト表示をデフォルトにする
-      NewWindowTarget = "Home"; # 新しい Finder ウィンドウをホームディレクトリにする
-    };
-
-    NSGlobalDomain = {
-      AppleInterfaceStyleSwitchesAutomatically = true; # ダークモードを自動で切り替える
-      InitialKeyRepeat = 15; # キーリピートの初期遅延を短くする
-      KeyRepeat = 2; # キーリピートの速度を速くする
-      _HIHideMenuBar = false; # メニューバーを常に表示
-    };
-
-    trackpad = {
-      Clicking = true; # タップでクリック
-      TrackpadRightClick = true; # 2本指で右クリック
-    };
-
-    screencapture = {
-      disable-shadow = true; # スクリーンショットの影を消す
-      type = "png"; # スクリーンショットのファイル形式
-    };
-
-    menuExtraClock.ShowSeconds = true; # メニューバーの時計に秒を表示
-  };
-
-  # Shell / Users
-  programs.zsh.enable = true;
-
-  users.users.${username} = {
-    home = homedir;
-    shell = pkgs.zsh;
-  };
-
-  system.stateVersion = 5;
-  system.primaryUser = username;
 }
